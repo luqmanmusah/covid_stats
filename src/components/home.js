909b775f91a1e46273fd958f971e63463a03ad4e
Luqman Musah <musahluqman@gmail.com>
2021-11-16 14:21:38 +0000

    Import header component into home

--- src/components/home.js
+++ src/components/home.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { NavLink } from 'react-router-dom';
 import { loadCountryThunk } from '../redux/home';
 import Header from './header';
 
@@ -15,6 +16,32 @@ export default function home() {
   return (
     <div>
       <Header />
+      <div>
+        <div>
+          <h2>Covid_19</h2>
+          <p>Number of Cases</p>
+          <p>in 2020-03-10</p>
+        </div>
+      </div>
+      <p>State By Country</p>
+      <ul>
+        {state.map((country) => (
+          <li>
+            <NavLink>
+              <div>
+                <div>
+                  <h2>{country.name}</h2>
+                  <p>
+                    {country.case}
+                    {' '}
+                    Cases
+                  </p>
+                </div>
+              </div>
+            </NavLink>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
